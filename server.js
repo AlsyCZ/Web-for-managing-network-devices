fe4f3a4f8b57d0ec2a0af69c9d011debdf726a06
Aleš Mrázek <100871677+AlsyCZ@users.noreply.github.com>
2025-03-05 13:33:56 +0100

    Web deploy 13

--- server.js
+++ server.js
@@ -92,7 +92,7 @@ if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, 'react-app/build')));
   
     app.get('*', (req, res) => {
-      res.sendFile(path.join(__dirname, 'react-app/build', 'index.html'));
+      res.sendFile(path.join(__dirname, 'react-app/build', 'index.js'));
     });
   }
 
