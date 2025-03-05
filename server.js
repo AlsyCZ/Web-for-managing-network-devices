22fb6293481c990b2477e40f4adb2b794295a8fa
Aleš Mrázek <100871677+AlsyCZ@users.noreply.github.com>
2025-03-05 14:22:33 +0100

    Web deploy 17

--- server.js
+++ server.js
@@ -64,10 +64,11 @@ connectToApi().catch(error => console.error('Initial connection error:', error))
 
 // MySQL database connection
 const db = mysql.createConnection({ 
-    host: 'localhost', 
-    user: 'root', 
-    password: '', 
-    database: 'projekt' 
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT
 });
 
 db.connect(err => {
