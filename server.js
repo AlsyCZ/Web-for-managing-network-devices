773e9ae41e0278be0867fceac9e1f040cb4b1886
Aleš Mrázek <100871677+AlsyCZ@users.noreply.github.com>
2025-03-05 14:00:36 +0100

    Web deploy 16

--- server.js
+++ server.js
@@ -649,9 +649,6 @@ process.on('SIGINT', async () => {
     process.exit();
 });
 
-app.listen(3001, () => {
-    console.log('Server is running at http://localhost:3001');
-});
 const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
